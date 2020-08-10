f80e26261490bfdf213bca9f4c0db68cc90c8437
Xiangrui Dong <samdongxiangrui@outlook.com>
2020-08-10 15:58:45 +0800

    [xdong] finish exercise 3-object

--- 3-object/values/index.js
+++ 3-object/values/index.js
@@ -1,3 +1,8 @@
 export default function countTypesNumber(source) {
   // TODO 6: 在这里写实现代码
+  let sum = 0;
+  Object.values(source).forEach(value => {
+    sum += parseInt(String(value), 10);
+  });
+  return sum;
 }
