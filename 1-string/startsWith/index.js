3c0f3886c7230c08d45d0162f0ee1d519526ff8f
Xiangrui Dong <samdongxiangrui@outlook.com>
2020-08-10 15:07:29 +0800

    [xdong] finish exercise 1-string

--- 1-string/startsWith/index.js
+++ 1-string/startsWith/index.js
@@ -1,3 +1,10 @@
 export default function collectCarNumberCount(collection) {
   // TODO 7: 在这里写实现代码
+  let count = 0;
+  collection.forEach(str => {
+    if (str.startsWith('粤A')) {
+      count += 1;
+    }
+  });
+  return count;
 }
