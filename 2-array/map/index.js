aa8ae745dcb8c69f7c0ebc851dbbbd8245393bda
Xiangrui Dong <samdongxiangrui@outlook.com>
2020-08-10 15:38:54 +0800

    [xdong] finish exercise 2-array

--- 2-array/map/index.js
+++ 2-array/map/index.js
@@ -1,3 +1,32 @@
 export default function numberMapToWord(collection) {
   // TODO 3: 在这里写实现代码
+  const alphabetCollection = [
+    'a',
+    'b',
+    'c',
+    'd',
+    'e',
+    'f',
+    'g',
+    'h',
+    'i',
+    'j',
+    'k',
+    'l',
+    'm',
+    'n',
+    'o',
+    'p',
+    'q',
+    'r',
+    's',
+    't',
+    'u',
+    'v',
+    'w',
+    'x',
+    'y',
+    'z'
+  ];
+  return collection.map(number => alphabetCollection[number - 1]);
 }
